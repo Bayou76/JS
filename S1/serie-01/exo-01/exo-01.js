efc166ee9df6fcc330cbb70438b8f41738303ba1
Bayou76 <103516275+Bayou76@users.noreply.github.com>
2022-07-21 17:11:31 +0200

    exos

---
+++ S1/serie-01/exo-01/exo-01.js
@@ -0,0 +1,4 @@
+let prenom = prompt("Votre prénom ? ");
+let age = parseInt(prompt("Votre âge ?"));
+age = age + 1
+console.log("Je m'appelle " + prenom + " et j'ai " + age + " ans l'année prochaine" );
