8e858b3dd78cc3d5805baec2737f2cbe8294ac38
Bayou76 <103516275+Bayou76@users.noreply.github.com>
2022-07-28 09:29:32 +0200

    maj

--- S5/TP/caracteristiques.js
+++ S5/TP/caracteristiques.js
@@ -1,48 +1,48 @@
 function force(event) {
 
-    let number = document.getElementById('force').name;
+    let number = document.getElementById('force').name + document.getElementById('force').value;
     let alea = Math.round(Math.random(1) * 20);
-    document.querySelector('.caractere').innerHTML = ` ${number} d${alea}`;
+    document.querySelector('.caractere').innerHTML =  ` ${number} = d${alea}`;
     alert(number + " d" + alea);
 
 
 }
 function dex(event) {
 
-    let number = document.getElementById('dex').name;
+    let number = document.getElementById('dex').name + document.getElementById('dex').value;
     let alea = Math.round(Math.random(1) * 20);
-    document.querySelector('.caractere').textContent = ` ${number} d${alea}`;
+    document.querySelector('.caractere').textContent =  ` ${number} = d${alea}`;
     alert(number + " d" + alea);
 }
 function con(event) {
 
-    let number = document.getElementById('con').name;
+    let number = document.getElementById('con').name + document.getElementById('con').value;
     let alea = Math.round(Math.random(1) * 20);
-    document.querySelector('.caractere').innerHTML = ` ${number} d${alea}`;
+    document.querySelector('.caractere').innerHTML =  ` ${number} = d${alea}`;
     alert(number + " d" + alea);
 
 }
 function sag(event) {
 
-    let number = document.getElementById('sag').name;
+    let number = document.getElementById('sag').name + document.getElementById('sag').value ;
     let alea = Math.round(Math.random(1) * 20);
-    document.querySelector('.caractere').innerHTML = ` ${number} d${alea}`;
+    document.querySelector('.caractere').innerHTML = ` ${number} = d${alea}`;
     alert(number + " d" + alea);
 
 }
 function cha(event) {
 
-    let number = document.getElementById('cha').name;
+    let number = document.getElementById('cha').name + document.getElementById('cha').value;
     let alea = Math.round(Math.random(1) * 20);
-    document.querySelector('.caractere').innerHTML = number + " d" + alea;
+    document.querySelector('.caractere').innerHTML =  ` ${number} = d${alea}`;
     alert(number + " d" + alea);
 
 }
 function int(event) {
 
-    let number = document.getElementById('int').name;
+    let number = document.getElementById('int').name + document.getElementById('int').value;
     let alea = Math.round(Math.random(1) * 20);
-    document.querySelector('.caractere').innerHTML = number + " d" + alea;
+    document.querySelector('.caractere').innerHTML =  ` ${number} = d${alea}`;
     alert(number + " d" + alea);
 
 }
---
+++ calculatrice/script.js
@@ -0,0 +1,15 @@
+function adition(a,b){
+    return a+b;
+}
+
+function soust(a,b){
+    return a-b;
+}
+
+function mult(a,b){
+    return a * b;
+}
+
+function division(a, b){
+    return a/b;
+}
